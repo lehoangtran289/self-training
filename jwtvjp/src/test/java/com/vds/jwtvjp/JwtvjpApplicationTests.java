bea7d01ec3dd7577866249c03dcc06ba448ab1d1
lehoangtran289 <lehoangtran289@gmail.com>
2021-07-23 18:02:01 +0700

    .

---
+++ jwtvjp/src/test/java/com/vds/jwtvjp/JwtvjpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vds.jwtvjp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtvjpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
