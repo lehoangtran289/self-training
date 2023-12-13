fdfc336331e0342b0c5133723958392eb83ac83a
htl982 <htl982@viettel.com.vn>
2023-12-13 09:41:43 +0700

    update version

--- springbootx/src/main/java/com/vds/springbootx/temp/ParentClass.java
+++ springbootx/src/main/java/com/vds/springbootx/temp/ParentClass.java
@@ -5,13 +5,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 @ConditionalOnProperty(value="parent", havingValue = "true")
 public class ParentClass {
     @Autowired
     protected JournalEntryRepository journalEntryRepository;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("ParentClass init");
+        save();
+    }
+
     protected void save() {
+        System.out.println(journalEntryRepository.findAll());
         System.out.println("ParentClass save");
     }
 }
--- springbootx/src/main/java/com/vds/springbootx/temp/SubClass.java
+++ springbootx/src/main/java/com/vds/springbootx/temp/SubClass.java
@@ -3,10 +3,20 @@ package com.vds.springbootx.temp;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 @ConditionalOnProperty(value="sub", havingValue = "true")
 public class SubClass extends ParentClass {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("SubClass init");
+        save();
+    }
+
     protected void save() {
+        System.out.println(journalEntryRepository.findAll());
         System.out.println("SubClass save");
     }
 }
